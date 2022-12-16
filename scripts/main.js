f8fb901a50de324a5b7bb9ab0b2396fe21d7291b
Davina Leong <davina.leong.sy@gmail.com>
2022-12-16 15:44:37 +0800

    WIP translate panes

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,8 @@
 console.log(`main.js loaded`)
+
+// Variables
+const panesEl = document.querySelector(`[data-element="panes"]`)
+
+const btnGoBackEl = document.querySelector(`[data-element="btn-go-back"]`)
+const btnNextStepEl = document.querySelector(`[data-element="btn-next-step"]`)
+const btnConfirmEl = document.querySelector(`[data-element="btn-confirm"]`)
